bbe47291b01ff49a8f1bf322df6e794bd51a2b54
EdgarMolard <edgar.mlrd@gmail.com>
2026-03-24 10:37:44 +0100

    Nettoyage input et exit/^d gèré

--- HandmadeShell.c
+++ HandmadeShell.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
     printf("Welcome to Handmade Shell!\n");
@@ -16,15 +17,17 @@ int main(){
         char *line = NULL;
         size_t size = 0;
         size_t len = getline(&line, &size, stdin);//On récupère la ligne de commande avec un string, sa taille pour la mémoire
-        if(len == -1){//Si l'input dans la ligne de commande c'est un exit,
+        
+        if (len > 0 && line[len - 1] == '\n') {//On nettoit l'input qui garde \n en fin de ligne
+            line[len - 1] = '\0';
+        }
+        
+        if(len == -1 || strcmp(line, "exit")==0){//Si l'input dans la ligne de commande c'est un exit,
             printf("Au revoir :p \n");
             free(line);//On libère line et
             exit(0);//On ferme le programme
         }
         
-        if (line[len - 1] == '\n') {
-            line[len - 1] = '\0';
-        }   
     }
     return 0;
 }
